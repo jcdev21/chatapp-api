6870b0ffedda2947800e7e59e3b5273b8e4e2b48
jcdev21 <jcdev21@gmail.com>
2024-02-06 10:28:30 +0700

    prisma global

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
-import { PrismaService } from 'src/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService, PrismaService, JwtService],
+  providers: [AuthService, UserService, JwtService],
 })
 export class AuthModule {}
