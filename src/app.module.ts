44938cd4140421056925609479f2e06a8b0a1a89
jcdev21 <jcdev21@gmail.com>
2024-02-16 16:22:14 +0700

    prisma global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma.module';
+import { GlobalModule } from './global.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule],
+  imports: [UserModule, AuthModule, GlobalModule],
   controllers: [AppController],
   providers: [AppService],
 })
