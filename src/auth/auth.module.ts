0f28521bff247d52d5058614663499c1b110d122
jcdev21 <jcdev21@gmail.com>
2024-02-17 23:44:41 +0700

    add auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService, JwtService],
+  providers: [
+    {
+      // membuat guard menjadi global
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    AuthService,
+    UserService,
+    JwtService,
+  ],
 })
 export class AuthModule {}
