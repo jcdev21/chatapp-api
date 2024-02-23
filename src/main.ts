ce33b89947eee80ec480bc2ce8eaa1eb3629cd2f
jcdev21 <jcdev21@gmail.com>
2024-02-23 14:32:56 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ZodFilter } from './utils/filters/zod.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ credentials: true, origin: 'http://localhost:5173' });
   app.use(helmet());
   app.use(compression());
   app.use(cookieParser());
