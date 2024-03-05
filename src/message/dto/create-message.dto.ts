bc5cee17487de1e7bf1c93328a0e90ce044e7476
jcdev21 <jcdev21@gmail.com>
2024-03-05 14:56:23 +0700

    change dto

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const createMessageSchema = z.object({
   chatId: z.string(),
   senderId: z.string(),
-  message: z.string(),
+  message: z.string().min(1),
 });
 
 export type CreateMessageDto = z.infer<typeof createMessageSchema>;
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const createUserSchema = z.object({
-  name: z.string(),
+  name: z.string().min(1),
   email: z.string().email(),
-  password: z.string(),
+  password: z.string().min(1),
 });
 
 export type CreateUserDto = z.infer<typeof createUserSchema>;
